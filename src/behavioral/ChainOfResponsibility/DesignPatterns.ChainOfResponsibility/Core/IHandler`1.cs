0b9e0422deb6a372503ad57b97ac3d82d181907a
Dimas de la Fuente <dimasdelafuen@gmail.com>
2021-02-25 23:43:50 +0100

    Chain Of Responsibility first approach (in progress)

---
+++ src/behavioral/ChainOfResponsibility/DesignPatterns.ChainOfResponsibility/Core/IHandler`1.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="IHandler`1.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace DesignPattern.ChainOfResponsibility.Core
+{
+    /// <summary>
+    /// The IChainHandler interface specifies the methods and properties
+    /// to be implemented by concrete Handlers.
+    /// </summary>
+    /// <typeparam name="TResponse">Type of handler's response object.</typeparam>
+    public interface IHandler<TResponse>
+    {
+        /// <summary>
+        /// The IChainHandler interface specifies the methods and properties
+        /// to be implemented by concrete Handlers.
+        /// </summary>
+        /// <returns>Execution result.</returns>
+        TResponse Execute();
+    }
+}
---
+++ src/behavioral/ChainOfResponsibility/DesignPatterns.ChainOfResponsibility/Implementation/MinorsDiscountHandlers.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="MinorsDiscountHandlers.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace DesignPattern.ChainOfResponsibility.Implementation
+{
+    using System;
+    using DesignPattern.ChainOfResponsibility.Models;
+
+    /// <summary>
+    /// Defines a specific implementation of IChainHandler that inherits from DiscountChainHandler.
+    /// </summary>
+    public class MinorsDiscountHandlers : DiscountChainHandler
+    {
+        /// <summary>
+        /// Executes Minors Discount chain handler logic.
+        /// </summary>
+        /// <returns>Purchase receipt.</returns>
+        public override PurchaseReceipt Execute()
+        {
+            return new PurchaseReceipt(); // TODO
+        }
+    }
+}
